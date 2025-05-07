fbf16675d1632e393b042d9a5c1dee026e919ded
Eric Sentongo <eric_sentongo@yahoo.com>
2025-05-07 09:25:42 +0300

    Apply .gitignore rules

--- app/theme/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    presets: ['babel-preset-expo'], // or whatever you're using
-    plugins: [
-        'tamagui',
-        'react-native-reanimated/plugin', // keep reanimated plugin LAST
-    ],
-};
